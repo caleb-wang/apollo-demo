5ee9e4d7ab8e940cc0521260874c74bc2c58a0d1
wanglongbin <17325640141@163.com>
2024-06-20 20:38:03 +0800

    apollo配置中心服务端源码与jar包提交

---
+++ apollo服务端启动程序/启动脚本.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "java -jar apollo-configservice-2.3.0-SNAPSHOT.jar"                    
+start cmd /c "java -jar apollo-adminservice-2.3.0-SNAPSHOT.jar"                    
+start cmd /c "java -jar apollo-portal-2.3.0-SNAPSHOT.jar"
+spause
